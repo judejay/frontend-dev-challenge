4cfc95031ac1dbcba918d03434ff5affe03020fc
judejay <judejay26@gmail.com>
2024-06-02 20:49:45 +0100

    refactor: to use form component

--- src/components/rfhZod.tsx
+++ src/components/rfhZod.tsx
@@ -12,88 +12,69 @@ import { DateTimePicker } from './ui/datetime-picker'
 import { Input } from './ui/input'
 
 
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form'
+
 type Inputs = z.infer<typeof FormDataSchema>
 
 export const RfhZod = () => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    watch,
-    setValue,
-    trigger,
-    formState: { errors },
-  } = useForm<Inputs>({
+  const formData = useForm<Inputs>({
     resolver: zodResolver(FormDataSchema),
   })
 
-  const [submittedData, setSubmittedData] =  useState<Inputs>()
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data)
-    reset
-    setSubmittedData(data)
-  }
+function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log("data", data)   
+    }
 
-console.log("errors", errors) ;
-console.log("wacth", watch('scheduledDeparture'))
 
   return (
     <section className='flex gap-6'>
-    <form onSubmit={handleSubmit(onSubmit)}>
-        <Label htmlFor="scheduledDeparture" className="text-right">
-            Departure
-        </Label>
-        <DateTimePicker 
-            isRequired 
-            onChange={(value) => {
-        setValue('scheduledDeparture', {
-                year: value.year, // Provide an initializer for the 'year' property
-                identifier: '',
-                era: '',
-                month: 0,
-                day: 0,
-                hour: 0,
-                minute: 0,
-                second: 0,
-                millisecond: 0
-        }, { shouldValidate: true });
-    }}
-            onBlur={() => trigger('scheduledDeparture')}
-            granularity="second" 
-        />
-        {errors.scheduledDeparture?.message && <p className='text-sm text-red-400'>{errors.scheduledDeparture.message}</p>}
+        <Form {...formData}>
+    <form onSubmit={formData.handleSubmit(onSubmit)}>
 
-    <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="scheduledArrival" className="text-right">
-              Arrival
-            </Label>
-            <DateTimePicker
-             isRequired 
-             onChange={(value) => {
-         setValue('scheduledArrival', {
-                 year: value.year, // Provide an initializer for the 'year' property
-                 identifier: '',
-                 era: '',
-                 month: 0,
-                 day: 0,
-                 hour: 0,
-                 minute: 0,
-                 second: 0,
-                 millisecond: 0
-         }, { shouldValidate: true });
-     }}
-             onBlur={() => trigger('scheduledDeparture')}
-            
-            granularity="second" />
-          </div>
-{errors.scheduledArrival?.message && <p className='text-sm text-red-400'>{errors.scheduledArrival.message}</p>}
-           <div className="grid grid-cols-4 items-center gap-4">
+<FormField
+          control={formData.control}
+          name="scheduledDeparture"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor="scheduledDeparture">scheduledDeparture</FormLabel>
+              <FormControl>
+                <DateTimePicker
+                  granularity="second"
+                  jsDate={field.value}
+                  onJsDateChange={field.onChange}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={formData.control}
+          name="scheduledArrival"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor="scheduledArrival">scheduledArrival</FormLabel>
+              <FormControl>
+                <DateTimePicker
+                  granularity="second"
+                  jsDate={field.value}
+                  onJsDateChange={field.onChange}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        
+        {formData.formState.scheduledDeparture?.message && <p className='text-sm text-red-400'>{formData.formState.scheduledDeparture.message}</p>}
+
+              <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="portOfLoading" className="text-right">
               Port of loading
             </Label>
-            <Input  {...register('portOfLoading')} />
-        {errors.portOfLoading?.message && <p className='text-sm text-red-400'>{errors.portOfLoading.message}</p>}
+            <Input  {...formData.register('portOfLoading')} />
+        {formData.formState.portOfLoading?.message && <p className='text-sm text-red-400'>{formData.formState.portOfLoading.message}</p>}
     
             </div>
 
@@ -101,26 +82,20 @@ console.log("wacth", watch('scheduledDeparture'))
             <Label htmlFor="portOfDischarge" className="text-right">
               Port of discharge
             </Label>
-            <Input  {...register('portOfDischarge')} />
-        {errors.portOfDischarge?.message && <p className='text-sm text-red-400'>{errors.portOfDischarge.message}</p>}
+            <Input  {...formData.register('portOfDischarge')} />
+        {formData.formState.portOfDischarge?.message && <p className='text-sm text-red-400'>{formData.formState.portOfDischarge.message}</p>}
             </div>
             <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="vessel" className="text-right">
               Vessel
             </Label>
-            <Input  {...register('vessel')} />
-        {errors.vessel?.message && <p className='text-sm text-red-400'>{errors.vessel.message}</p>}
-
+            <Input  {...formData.register('vessel')} />
+        {formData.formState.vessel?.message && <p className='text-sm text-red-400'>{formData.formState.vessel.message}</p>}
             </div>
-
-     
-       
+    
       <button type="submit">Submit</button>
-
-      {submittedData && (
-        <pre>{JSON.stringify(submittedData, null, 2)}</pre>
-      )}
     </form>
+    </Form>
     </section>
   )
 }
--- src/lib/schema.tsx
+++ src/lib/schema.tsx
@@ -1,28 +1,8 @@
 import { z } from 'zod';
  
  export const FormDataSchema = z.object({
-  scheduledDeparture: z.object({
-    identifier: z.string(),
-    era: z.string(),
-    year: z.number(),
-    month: z.number(),
-    day: z.number(),
-    hour: z.number(),
-    minute: z.number(),
-    second: z.number(),
-    millisecond: z.number(),
-  }),
-  scheduledArrival: z.object({
-    identifier: z.string(),
-    era: z.string(),
-    year: z.number(),
-    month: z.number(),
-    day: z.number(),
-    hour: z.number(),
-    minute: z.number(),
-    second: z.number(),
-    millisecond: z.number(),
-  }),
+  scheduledDeparture:z.date().nullable() ,
+  scheduledArrival: z.date().nullable() ,
   portOfLoading: z.string().nonempty('Port of Loading is required.'),
   portOfDischarge: z.string().nonempty('Port of Discharge is required.'),
   vessel: z.string().nonempty('Vessel is required.'),
